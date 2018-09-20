075e34cd8d05ca4a1082ac115f1a0a54bd74da7e
KVTKS <tkszxz@gmail.com>
2018-09-20 16:04:56 +0800

    first commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,24 @@
+import {Component} from "@angular/core";
+import {NavController, AlertController, ToastController, MenuController} from "ionic-angular";
+import {HomePage} from "../home/home";
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  constructor(public nav: NavController, public forgotCtrl: AlertController, public menu: MenuController, public toastCtrl: ToastController) {
+    this.menu.swipeEnable(false);
+  }
+
+  
+
+  // login and go to home page
+  login() {
+    this.nav.setRoot(HomePage);
+  }
+
+
+
+}
